3fa9975c841e24d6f120586c3c59c697f1648daa
darktend <vladgodunqwer@gmail.com>
2021-03-02 00:30:10 +0200

    i donte task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+   
+    if (n >= 0) {
+        return n.toString().split('').reverse().join('')
+      } else{
+        return (-n).toString().split('').reverse().join('')
+      }
+      
+    
+                    
 }
